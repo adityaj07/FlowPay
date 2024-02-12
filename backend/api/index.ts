3510dda84beff7dc70ab608c037627894aa7c21e
Aditya Joshi <joshiaditya.aj07@gmail.com>
2024-02-12 20:37:21 +0530

    fix: api/index.ts

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -11,12 +11,7 @@ app.use(
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", env.FRONTEND_URL);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT,UPDATE, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+
 app.use(express.json());
 app.use(cookieParser());
 
